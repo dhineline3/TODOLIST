7a6de4c277c4ba26cc050841ce76ecc7de006bdb
Daniel Hineline <danielhineline3@gmail.com>
2019-03-05 21:32:45 -0500

    FINAL

--- TODOWeeklongProject/Program.cs
+++ TODOWeeklongProject/Program.cs
@@ -10,8 +10,22 @@ namespace TODOWeeklongProject
     {
         static void Main(string[] args)
         {
-            List<string> optionList = new List<string>(); //Inititiating a list
-            
+            String input = Console.ReadLine();
+            List<String> userList = new List<String>(); //Inititiating a list
+            Console.WriteLine("Welcome to your To Do List!");
+            Console.WriteLine("Please select an option: ");
+            Console.WriteLine("1. Press 1 to add an item to your To Do List.");
+            Console.WriteLine("2. Press 2 to view the entire To Do List.");
+            Console.WriteLine("3. Press 3 to exit now.");
+            //^These are the drop down menu options
+
+            userList.Add(input);
+            String inputNewUser = Console.ReadLine();
+            userList.Add(inputNewUser);
+            foreach (var user in userList)
+            {
+                Console.WriteLine(userList);
+            }
         }
     }
 }
